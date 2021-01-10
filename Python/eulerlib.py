39ce0550e629ff7189e2ef9fbc3b3d648d2a905b
Mattis <mattis.martens@gmail.com>
2021-01-10 22:19:42 +0100

    Update eulerlib.py

--- Python/eulerlib.py
+++ Python/eulerlib.py
@@ -1,11 +1,3 @@
-# 
-# Shared code for solutions to Project Euler problems
-# Copyright (c) Project Nayuki. All rights reserved.
-# 
-# https://www.nayuki.io/page/project-euler-solutions
-# https://github.com/nayuki/Project-Euler-solutions
-# 
-
 import array, math
 
 
