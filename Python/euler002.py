441e14f535c4f85bcc828dffe4af4f7b02dd2952
Mattis <mattis.martens@gmail.com>
2021-01-11 11:41:04 +0100

    Cleaning up

--- Python/euler002.py
+++ Python/euler002.py
@@ -1,22 +1,26 @@
-lst = []
-lst1 = []
-a = 0
-lst.append(a)
-b = 1
-lst.append(b)
-c = 1
-lst.append(c)
-while a < 4000000 and b < 4000000 and c < 4000000:
-    a = b + c
+def main():
+    lst = []
+    lst1 = []
+    a = 0
     lst.append(a)
-    b = c + a
+    b = 1
     lst.append(b)
-    c = a + b
+    c = 1
     lst.append(c)
-for num in lst:
-    if num%2==0:
-        lst1.append(num)
+    while a < 4000000 and b < 4000000 and c < 4000000:
+        a = b + c
+        lst.append(a)
+        b = c + a
+        lst.append(b)
+        c = a + b
+        lst.append(c)
+    for num in lst:
+        if num%2==0:
+            lst1.append(num)
 
-d = sum(lst1)
-print(d)
+    d = sum(lst1)
+    return str(d)
+
+if __name__ == "__main__":
+    print(main())
 
--- Python/euler009.py
+++ Python/euler009.py
@@ -1,14 +1,19 @@
 from math import *
-a = 0
-b = 0
-c = 0
-while a < 1000:
-    a = a + 1
+
+def main():
+    a = 0
     b = 0
-    while b < 1000:
-        b = b + 1
-        c = sqrt(a**(2) + b**(2))
-        if a + b + c == 1000:
-            d = a*b*c
-            break
-print(d)
+    c = 0
+    while a < 1000:
+        a = a + 1
+        b = 0
+        while b < 1000:
+            b = b + 1
+            c = sqrt(a**(2) + b**(2))
+            if a + b + c == 1000:
+                d = a*b*c
+                break
+    return str(int(d))
+
+if __name__ == "__main__":
+    print(main())
