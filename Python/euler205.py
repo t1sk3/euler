14ecccce30b3269fe0375fc2ef0cf93e1adda325
Mattis <mattis.martens@gmail.com>
2021-01-19 22:22:21 +0100

    Update euler205.py

--- Python/euler205.py
+++ Python/euler205.py
@@ -1,12 +1,15 @@
 
 
 def main():
+    # creating dices
     peter = [1,2,3,4]
     colin = [1,2,3,4,5,6]
     peterRes = []
     colinRes = []
     peterChance = []
     colinChance = []
+
+    # creating all peters possible combinations
     for i in peter:
         for j in peter:
             for k in peter:
@@ -18,6 +21,7 @@ def main():
                                     for q in peter:
                                         peterRes.append(i+j+k+l+m+n+o+p+q)
     
+    # creating all colins possible combinations
     for i in colin:
         for j in colin:
             for k in colin:
@@ -26,19 +30,21 @@ def main():
                         for n in colin:
                             colinRes.append(i+j+k+l+m+n)
     
+    # counting how many times a combination occurs
     for i in range(1,37):
         x = peterRes.count(i)
         y = colinRes.count(i)
         peterChance.append([i, x/len(peterRes)])
         colinChance.append([i, y/len(colinRes)])
 
-    for i in peterChance:
-        res = 0
-        for j in colinChance:
+    for i in peterChance:                   # When a throw of Peter is higher than Colins,
+        res = 0                             # the chance of this throw winning will be equal to
+        for j in colinChance:               # the chance of this throw * the sum of the chances of the throws it wins of Colin
             if i[0] > j[0]:
                 res += i[1]*j[1]
         i.append(res)
     
+    # adds up all calculated chances
     chance = 0
     for i in peterChance:
         chance += i[2]
