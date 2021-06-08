c11e6873e6405a1c97b5cc5d7f53868aa3d4a7b0
t1sk3 <76489233+t1sk3@users.noreply.github.com>
2021-06-08 18:31:07 +0200

    Update prime_numbers.go
    IsPrime is updated to be able to take int, uint, int64 and uint64

--- GO/eulerlib/prime_numbers.go
+++ GO/eulerlib/prime_numbers.go
@@ -5,18 +5,18 @@ import (
 )
 
 // checks to see if the given number is a prime
-func IsPrime(p int) bool {
-	end := int(math.Sqrt(float64(p)))
-	if end*end == p {
-		return false
+func IsPrime(a interface{}) bool {
+	switch v := a.(type) {
+	case int:
+		return isPrimeInt(v)
+	case uint:
+		return isPrimeUint(v)
+	case int64:
+		return isPrimeInt64(v)
+	case uint64:
+		return isPrimeUint64(v)
 	}
-	end++
-	for i := 2; i < end; i++ {
-		if p%i == 0 {
-			return false
-		}
-	}
-	return true
+	return false
 }
 
 func ListPrimality(n int) []bool {
@@ -32,4 +32,60 @@ func ListPrimality(n int) []bool {
 		}
 	}
 	return res
+}
+
+func isPrimeInt(p int) bool {
+	end := int(math.Sqrt(float64(p)))
+	if end*end == p {
+		return false
+	}
+	end++
+	for i := 2; i < end; i++ {
+		if p%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func isPrimeUint(p uint) bool {
+	end := uint(math.Sqrt(float64(p)))
+	if end*end == p {
+		return false
+	}
+	end++
+	for i := uint(2); i < end; i++ {
+		if p%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func isPrimeInt64(p int64) bool {
+	end := int64(math.Sqrt(float64(p)))
+	if end*end == p {
+		return false
+	}
+	end++
+	for i := int64(2); i < end; i++ {
+		if p%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func isPrimeUint64(p uint64) bool {
+	end := uint64(math.Sqrt(float64(p)))
+	if end*end == p {
+		return false
+	}
+	end++
+	for i := uint64(2); i < end; i++ {
+		if p%i == 0 {
+			return false
+		}
+	}
+	return true
 }
