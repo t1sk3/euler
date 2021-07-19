b9faf796af43ab4b985b34d1b31e1c7788713f57
t1sk3 <76489233+t1sk3@users.noreply.github.com>
2021-07-19 20:35:59 +0200

    Update euler491.go

--- GO/euler491.go
+++ GO/euler491.go
@@ -14,7 +14,6 @@ func main() {
 }
 
 func solve() (res int64) {
-	//NUMS := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 	NUMSSTRING := []string{"0", "0", "1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9"}
 	var tmp float64
 	var lst []int
