79b97b8e778d9c2fab222b9cb82194c6c497bce3
t1sk3 <76489233+t1sk3@users.noreply.github.com>
2021-07-27 21:44:07 +0200

    addition of euler250.go

---
+++ GO/euler250.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"eulerlib"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(solve())
+}
+
+func solve() uint64 {
+	MODULUS := uint64(10000000000000000)
+
+	numSubs := getUint64ListOfLength(250)
+	numSubs[0] = 1
+	var tmp int
+	var newSlice []uint64
+
+	for i := 1; i <= 250250; i++ {
+		tmp = int(eulerlib.PowMod(i, i, 250))
+		newSlice = getUint64ListOfLength(250)
+		for j := 0; j < 250; j++ {
+			newSlice[(j+tmp)%250] = (numSubs[j] + numSubs[(j+tmp)%250]) % MODULUS
+		}
+		numSubs = newSlice
+	}
+	return (numSubs[0] - 1 + MODULUS) % MODULUS
+}
+
+func getUint64ListOfLength(n int) (res []uint64) {
+	for i := 0; i < n; i++ {
+		res = append(res, 0)
+	}
+	return
+}
+
+func getLength(s []int64) (res int) {
+	for _, e := range s {
+		if e != 0 {
+			res++
+		}
+	}
+	return
+}
--- GO/eulerlib/num_theory.go
+++ GO/eulerlib/num_theory.go
@@ -4,6 +4,7 @@ import (
 	"math"
 	"strconv"
 	"math/bits"
+	"math/big"
 )
 
 // returns the number of divisors the given integer has
@@ -210,4 +211,11 @@ func PrimeFactors(n int) []int {
 	}
 
 	return primefs
+}
+
+func PowMod(x int, y int, m int) int64 {
+	x1 := new(big.Int).SetInt64(int64(x))
+	y1 := new(big.Int).SetInt64(int64(y))
+	m1 := new(big.Int).SetInt64(int64(m))
+	return x1.Exp(x1, y1, m1).Int64()
 }
